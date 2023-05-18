6d09be2edc023d14fb15034f4144d3ded9731726
Prasanjeet <prasanjeetcharan.pc@gmail.com>
2023-05-18 22:46:58 +0530

    dptechlabs

---
+++ index.js
@@ -0,0 +1,3 @@
+document.getElementsByClassName("our_moto").innerHTML = "OUR MOTTO Where Innovation Meets Education!";
+document.querySelector("p").style.backgroundColor = "green";
+document.querySelector("p").style.padding = "20px";
